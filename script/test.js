9a2f3313fe671829522390c5169551547a478782
MariaAlba1306 <maria.alba@wallapop.com>
2022-04-03 19:08:59 +0200

    json ended with reload and new screens

--- script/test.js
+++ script/test.js
@@ -1,14 +1,19 @@
 //Declare global vars for facilitate accesibility
+//User values
+var userName;
 //JSON values
 var questions = [];
 var actualQuestion;
 var actualWord;
 var actualResponse;
+var actualResult;
+var restantQuestion;
+var pregunta;
 
 //variadito
 var startingQuestion = 0;
-var startingAnswer = 0;
-var endQuestion = 6;
+var numberOfQuestions;
+var endingQuestion;
 
 //Images correct/incorrect
 var imgCorrect = "correct";
@@ -20,34 +25,25 @@ var check = "check";
 
 //Round
 var round = 1;
-var play = {
-  numberround: round,
-  responses: [
-    {
-      correctAnswers: [],
-      incorrectAnswers: [],
-      nullAnswers: [],
-    },
-  ],
-  numberround: round,
-  responses: [
-    {
-      correctAnswers: [],
-      incorrectAnswers: [],
-      nullAnswers: [],
-    },
-  ],
-};
 //Final answers
-var correctAnswers = play.responses[0].correctAnswers;
-var incorrectAnswers = play.responses[0].incorrectAnswers;
-var nullAnswers = play.responses[0].nullAnswers;
+var incorrectAnswers = 0;
+var nullAnswers = 0;
+var correctAnswers = 0;
 
 //STEPS
+function InitialModal() {
+  userName = document.getElementById("name").value;
+  if (userName == "") {
+    alert("Nombre obligatorio");
+  } else {
+    document.querySelector("#startGame").style.display = "none";
+    document.querySelector("#game").style.display = "block";
+  }
+  document.querySelector("#welcome").innerText = "Bienvenido, " + userName;
+}
+
 //1. Execute the lookup in JSON
-
 fetchQuestions();
-
 // ---> Functions
 function fetchQuestions() {
   //retrieve json question
@@ -55,28 +51,13 @@ function fetchQuestions() {
     .then((response) => response.json())
     .then((data) => {
       questions = data;
-      initValues();
-      console.log(play);
+      showQuestion();
     });
-}
-function initValues() {
-  //refresh global variables
-  actualQuestion = questions.question[startingQuestion].question;
-  actualWord = questions.question[startingQuestion].word;
-  actualResponse = questions.question[startingQuestion].response;
-  item = "item" + questions.question[startingQuestion].word;
 
-  //set global vars values onto html
-  document.querySelector("#word").innerText = "Con la...." + actualWord;
-  document.querySelector("#question").innerText = actualQuestion;
-  questionNumber();
-  console.log("pregunta nº ", startingQuestion);
-  console.log("respuesta nº ", startingQuestion);
-
-  console.log(play);
-
-  //roundAnswers.push(questions.question[startingAnswer].word);
-  //console.log(roundAnswers);
+  // var filteredResults = questions.filter(function (entry) {
+  //   entry.question.word == "A";
+  // });
+  // console.log(filteredResults);
 }
 
 //2. Get user Response when clicking in send
@@ -94,103 +75,152 @@ function getUserResponse() {
 function checkResults() {
   //Compare both responses when clicks
   var userResponse = getUserResponse();
-
   //If user's answer = actualResponse
   if (userResponse === actualResponse) {
-    console.log("respuesta: correcto"); //delete this. Added to see
-
-    //Change letter color
-
-    document.querySelector(`#${item}`).classList.remove("item");
-    document.querySelector(`#${item}`).classList.add("item--ok");
-    //document
-    // .querySelector(".circle .item")
-    // .eq(questions.question[startingAnswer].word)
-    //  .addClass("item--failure");
-
-    //Add number to question count to go to next question
-    if (!correctAnswers.includes(questions.question[startingAnswer].word)) {
-      correctAnswers.push(questions.question[startingAnswer].word);
-    }
-    console.log(play);
+    correctResponse();
     //If user's answer  is blank
   } else if (userResponse === "no_answer") {
-    console.log("respuesta:", userResponse);
-
-    //Change letter color
-
-    document.querySelector(`#${item}`).classList.remove("item");
-    document.querySelector(`#${item}`).classList.add("item--null");
-
-    if (!nullAnswers.includes(questions.question[startingAnswer].word)) {
-      nullAnswers.push(questions.question[startingAnswer].word);
-    }
-    console.log(play);
-
+    blankResponse();
     //If user's answer != actualResponse
   } else {
-    console.log("respuesta incorrecta:", userResponse);
-
-    if (!incorrectAnswers.includes(questions.question[startingAnswer].word)) {
-      incorrectAnswers.push(questions.question[startingAnswer].word);
-    }
-    console.log(play);
-    //Change letter color
-
-    document.querySelector(`#${item}`).classList.remove("item");
-    document.querySelector(`#${item}`).classList.add("item--failure");
+    incorrectResponse();
   }
   //Delete user's response for next question
+  changeColor();
   document.getElementById("myText").value = "";
+  document.querySelector(`#${item}`).classList.remove("item--null");
 
   //Add time for next question
   setTimeout(nextQuestion, 750);
-
-  gatheringresults();
-  finalCheck();
 }
+function correctResponse() {
+  getUserResponse();
+  questions.question[startingQuestion].result = 1;
+  correctAnswers = correctAnswers + 1;
+
+  //Change letter color
+
+  //document
+  // .querySelector(".circle .item")
+  // .eq(questions.question[startingQuestion].word)
+  //  .addClass("item--failure");
+
+  //Add number to question count to go to next question
+}
+function blankResponse() {
+  console.log(startingQuestion);
+  getUserResponse();
+  //  questions.question[startingQuestion].result = 0;
+  questions.question[startingQuestion].result = 0;
+  //Change letter color
+  nullAnswers = nullAnswers + 1;
+}
+function incorrectResponse() {
+  getUserResponse();
+  questions.question[startingQuestion].result = 2;
+  //Change letter color
+  incorrectAnswers = incorrectAnswers + 1;
+}
+
 //4. Goes to next question
+function reload() {
+  // if (isAllQuestionsAnswered() == false) {
+  startingQuestion = 0;
+  round++;
+  // } else {
+  //   document.querySelector("#game").style.display = "none";
+  //   document.querySelector("#modal").style.display = "block";
+  //   summary();
+  // }
+}
+function changeColor() {
+  if (questions.question[startingQuestion].result == 2) {
+    document.querySelector(`#${item}`).classList.remove("item");
+    document.querySelector(`#${item}`).classList.add("item--failure");
+  } else if (questions.question[startingQuestion].result == 1) {
+    document.querySelector(`#${item}`).classList.remove("item");
+    document.querySelector(`#${item}`).classList.add("item--ok");
+  } else {
+    // document.querySelector(`#${item}`).classList.remove("item-null");
+    // document.querySelector(`#${item}`).classList.add("item--null");
+  }
+}
 
 function nextQuestion() {
-  //Next question functiom
-  var NumberofQuestions = questions.question.length;
-  questionNumber();
-  //Delete img for next question
-  document.querySelector(`#${imgCorrect}`).style.display = "none";
-  document.querySelector(`#${imgIncorrect}`).style.display = "none";
+  if (startingQuestion == endingQuestion - 1) {
+    reload();
+  } else {
+    startingQuestion++;
+    console.log(`#${item}`);
+  }
+  showQuestion();
+}
+function showQuestion() {
+  console.log(startingQuestion);
+  actualResult = questions.question[startingQuestion].result;
+  endingQuestion = questions.question.length;
+  isAllQuestionsAnswered();
 
-  actualQuestion = questions.question[startingQuestion].question;
-  actualWord = questions.question[startingQuestion].word;
-  actualResponse = questions.question[startingQuestion].response;
-  item = "item" + questions.question[startingQuestion].word;
+  if (actualResult == 0) {
+    // Next question functiom
+    actualQuestion = questions.question[startingQuestion].question;
+    actualWord = questions.question[startingQuestion].word;
+    actualResponse = questions.question[startingQuestion].response;
+    item = "item" + questions.question[startingQuestion].word;
+    document.querySelector(`#${item}`).classList.add("item--null");
+  } else {
+    if (isAllQuestionsAnswered() == false) {
+      nextQuestion();
+    } else {
+      summary();
+    }
+  }
   //set global vars values onto html
-  document.querySelector("#word").innerText = "Con la...." + actualWord;
   document.querySelector("#question").innerText = actualQuestion;
 }
 
 //FINAL CHECK AND RESTART
-
-function gatheringresults() {
-  userResponse = getUserResponse();
-  startingQuestion;
-  // gathering results
-  if (userResponse === actualResponse) {
-    startingAnswer++;
-    startingQuestion++;
-  } else if (userResponse === "no_answer") {
-    startingQuestion++;
-    startingAnswer++;
+function isAllQuestionsAnswered() {
+  var answeredQuestion = questions.question.filter(function (question) {
+    return question.result != 0;
+  });
+  console.log(answeredQuestion.length);
+  // answeredQuestion == completed responses
+  if (answeredQuestion.length == endingQuestion) {
+    return true;
   } else {
-    startingQuestion++;
-    startingAnswer++;
+    return false;
   }
-  reload();
-  console.log("Pregunta nº ", startingQuestion);
-  console.log("respuesta nº ", startingAnswer);
+}
+function startTimer(duration, display) {
+  var timer = duration,
+    minutes,
+    seconds;
+  setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      summary();
+      // timer = 0;
+    }
+  }, 1000);
 }
 
-function finalCheck() {
-  //Number of final results
+window.onload = function () {
+  var fiveMinutes = 60 * 1,
+    display = document.querySelector("#time");
+  startTimer(fiveMinutes, display);
+};
+
+function summary() {
+  document.querySelector("#game").style.display = "none";
+  document.querySelector("#modal").style.display = "block";
 
   document.querySelector("#responsesok").innerText =
     "correctas:" + correctAnswers;
@@ -199,41 +229,3 @@ function finalCheck() {
     "incorrectas:" + incorrectAnswers;
   document.querySelector("#round").innerText = "round:" + round;
 }
-
-function questionNumber() {
-  document.querySelector("#questionNumber").innerText = startingQuestion + 1;
-}
-
-function reload() {
-  if (startingQuestion === endQuestion) {
-    startingQuestion = 0;
-    startingAnswer = 0;
-    round++;
-    document.querySelector(`#${item}`).classList.remove("item-null");
-    document.querySelector(`#${item}`).classList.add("item--ok");
-  }
-}
-
-//NEEEEEWWWWWWWWWWWWWW
-
-// var play = {
-//   round: round,
-//   responses: [
-//     {
-//       correctAnswers: [],
-//       incorrectAnswers: [],
-//       nullAnswers: [],
-//     },
-//   ],
-// };
-// console.log(play);
-// console.log(play.responses[0].correctAnswers[0]);
-// function test() {
-//   play.responses[0].correctAnswers.push(
-//     questions.question[startingAnswer].word
-//   );
-//   play.responses[0].incorrectAnswers.push("A");
-//   console.log(play.responses[0]);
-//   console.log(play.responses[0].correctAnswers[1]);
-// }
-// test();
